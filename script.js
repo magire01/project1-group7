3f1d56a4fa13a4c60d6d6447d3ec1da8a8b3a8d5
Ty Hunt <jtmhunt22@gmail.com>
2020-07-09 13:54:44 -0600

    got the api for the cocktail

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-var testing = "testing the Jquery";
+// * displays the functions that we are calling
+function displayCocktails() {
+    // event.preventDefault()
+    // var cocktailSearch = $("#search-bar").val() I dont know the id of the search bar yet
+    var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka`
+    //get the ajax call for the ui
+    $.ajax({
+        url: cocktailURL,
+        method: "GET"
+    }).then(function (response) {
+        results = response.drinks;
+        for (var i = 0; i < results.length; i++){
+        }
+        // ! create a div
+        // ! Do we want each drink to be in its own Card?
+        // ! create a list of the different drinks
+        // ! display the image of the drinks 
+        // ! display the title of the drink
+        // ! display the ingredients of the drink
+        // ! display the instructions of how to make the drink
+    })
+}
 
-console.log(testing);
-
-$("#testingDiv").append(testing)
+displayCocktails()
+// $("#search-button").on("click", displayCocktails) idk what the search button id looks like yet
