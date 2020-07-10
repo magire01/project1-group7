d7d178afdae73d8331ad8ce5a6c978a25c2df0b4
Mark Gire <magire01@gmail.com>
2020-07-10 16:48:33 -0400

    Search cocktail recipe

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ var cocktailSearchResults = $("#drinks-results");
 // * displays the functions that we are calling
 
 function displayRecipes(){
-    var ingredient1 = "eggs"
-    var apiURL = "https://api.spoonacular.com/food/products/search?query=" + ingredient1 + "&apiKey=2c42667b7c2a405bbf07446e5d8a2887";
+    var ingredient1 = "egg"
+    var apiURL = "https://api.spoonacular.com/food/products/search?query=" + ingredient1 + "&apiKey=26ac3ffbd7f44fecab6782f58704ed4e";
     $.ajax({
         url: apiURL,
         method:"GET"
@@ -24,39 +24,44 @@ function displayRecipes(){
 }
 
 
-// //CocktailDB Query Builder
-// function buildQueryCocktails(cocktailIngredient) {
-//     var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailIngredient}`
-//     console.log(cocktailURL);
-//     return cocktailURL;
+// // //CocktailDB Query Builder
+// // function buildQueryCocktails(cocktailIngredient) {
+// //     var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailIngredient}`
+// //     console.log(cocktailURL);
+// //     return cocktailURL;
+// // }
+
+// //CocktailDB AJAX Call
+// function displayCocktails(ingredientDrink) {
+//     // event.preventDefault()
+//     //  var cocktailSearch = $("#search-bar-drinks").val() 
+//     var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${ingredientDrink}&apiKey=1`
+//     //get the ajax call for the ui
+//     $.ajax({
+//         url: cocktailURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(response);
+
+        
+//     })
 // }
 
-//CocktailDB AJAX Call
-function displayCocktails(ingredientDrink) {
-    // event.preventDefault()
-    //  var cocktailSearch = $("#search-bar-drinks").val() 
-    var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${ingredientDrink}&apiKey=26ac3ffbd7f44fecab6782f58704ed4e`
-    //get the ajax call for the ui
-    $.ajax({
-        url: cocktailURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+// // function makeCocktailBtn() {
+// //     var cocktailResultBtn = 
+// // }
 
-    })
-}
+// //CocktailDB Search Button
+// cocktailSearchBtn.on("click", function(event) {
+//     event.preventDefault();
 
-//CocktailDB Search Button
-cocktailSearchBtn.on("click", function(event) {
-    event.preventDefault();
+//     var cocktailIngredient = $("#search-bar-drinks").val();
 
-    var cocktailIngredient = $("#search-bar-drinks").val();
-
-    displayCocktails(cocktailIngredient);
+//     displayCocktails(cocktailIngredient);
     
-    console.log("ingredient-drink: " + cocktailIngredient)
+//     console.log("ingredient-drink: " + cocktailIngredient)
 
-});
+// });
 
 // //CocktailDB Generate Search Results
 
@@ -64,6 +69,6 @@ cocktailSearchBtn.on("click", function(event) {
     
 // }
 
-// displayRecipes();
+displayRecipes();
 
 
