275628549162776861c297e08b5aded2435be7ae
Mark Gire <magire01@gmail.com>
2020-07-11 02:46:17 -0400

    Search bar pulls 5 search result buttons with click events

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
-// var testing = "testing the Jquery";
+$("#search-button-recipe").on("click", function (event) {
 
-// console.log(testing);
+    event.preventDefault();
 
-// $("#testingDiv").append(testing)
+    var ingredient1 = $("#search-bar-recipe").val();
+
+    displayToPage(ingredient1)
+})
+
+$(document).on("click", ".newRecipes", function (event){
+
+    var recipe =$(this)[0].innerHTML;
+
+    console.log(recipe)
+
+})
 
 
-var cocktailSearchValue = $("#search-bar-drinks").val();
-var cocktailSearchBtn = $("#search-button-drinks");
-var cocktailSearchResults = $("#drinks-results");
 
+function displayToPage(ingredient){
+    
+        var apiURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + "&number=5&apiKey=124a6ebafffa4ec9a8b75c752871ee40";
 
-// * displays the functions that we are calling
+        $.ajax({
+            url: apiURL,
+            method:"GET"
+        }).then(function(response) {
 
-function displayRecipes(){
-    var ingredient1 = "egg"
-    var apiURL = "https://api.spoonacular.com/food/products/search?query=" + ingredient1 + "&apiKey=26ac3ffbd7f44fecab6782f58704ed4e";
+            console.log(response)
+            
+            for (i = 0; i < 5; i++){
+
+                var recipeBtn = $("<button class = newRecipes>").text(response[i].title);
+
+                $("#search-results-recipe").append(recipeBtn)
+
+            }
+        });
+        
+    } 
+
+//CocktailDB Search
+//Click event for "#search-button-drinks"
+$("#search-button-drinks").on("click", function(event) {
+     event.preventDefault();
+     //variable for value of search bar
+     var ingredientDrinks = $("#search-bar-drinks").val();
+     //Call displayCocktails (AJAX function for cocktailDB) allowing value of search to be used in queryURL
+     displayCocktails(ingredientDrinks)
+})
+
+//Click events for each generated "#search-results-drinks" button
+$(document).on("click", ".newDrinks", function (event){
+    //Variable for value of button text (in this case the recipe name pulled from API)
+    var drink = $(this)[0].innerHTML;
+    //Button Name test
+    console.log(drink);
+})
+
+//AJAX function for TheCocktailDB API (Input is dummy variable to pass "ingredientDrinks")
+function displayCocktails(input) {
+    //URL link which will pass "input" AKA ingredientDrinks
+    var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}+&apiKey=1`
+    console.log(cocktailURL);
+    //Call to AJAX 
     $.ajax({
-        url: apiURL,
-        method:"GET"
-    }).then(function(response) {
+        url: cocktailURL,
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
-    });
+        //Loop to create "#search-results-drink" buttons
+        for (var j = 0; j < 5; j++) {
+            console.log(response.drinks[0].strDrink)
+            var drinksBtn = $("<button>");
+            //Class = "newDrinks" for styling and click event targeting purposes
+            drinksBtn.attr("class", "newDrinks")
+            //Assign button text for Cocktail name from API
+            drinksBtn.text(response.drinks[j].strDrink);
+            //Append to "#search-results-drink" DIV
+            $("#search-results-drink").append(drinksBtn);
+        }
+    })
 }
 
 
-// // //CocktailDB Query Builder
-// // function buildQueryCocktails(cocktailIngredient) {
-// //     var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailIngredient}`
-// //     console.log(cocktailURL);
-// //     return cocktailURL;
-// // }
 
-// //CocktailDB AJAX Call
-// function displayCocktails(ingredientDrink) {
-//     // event.preventDefault()
-//     //  var cocktailSearch = $("#search-bar-drinks").val() 
-//     var cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${ingredientDrink}&apiKey=1`
-//     //get the ajax call for the ui
-//     $.ajax({
-//         url: cocktailURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response);
-
-        
-//     })
-// }
-
-// // function makeCocktailBtn() {
-// //     var cocktailResultBtn = 
-// // }
-
-// //CocktailDB Search Button
-// cocktailSearchBtn.on("click", function(event) {
-//     event.preventDefault();
-
-//     var cocktailIngredient = $("#search-bar-drinks").val();
-
-//     displayCocktails(cocktailIngredient);
-    
-//     console.log("ingredient-drink: " + cocktailIngredient)
-
-// });
-
-// //CocktailDB Generate Search Results
-
-// function generateCocktailResults() {
-    
-// }
-
-displayRecipes();
 
 
